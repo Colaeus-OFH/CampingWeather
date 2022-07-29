da2018fe95ab2c795b5cff28cf4c27c6c7c68cc5
Doug Merrett <dougmerrett@ofh.ca>
2022-07-29 13:33:12 +0000

    Straightened out multiple plots on same dash. Removed extra tabs.

--- app.R
+++ app.R
@@ -3,13 +3,23 @@
 library(shinydashboard)
 library(timeDate)
 library(dplyr)
+#library(tidyverse)
 library(ggplot2)
 library(shiny)
 library(leaflet)
+library(DBI)
+library(RSQLite)
+# library(RSocrata)
+# library(openair)
 
 # Get todays date for plot
 setRmetricsOptions(myFinCenter = "Halifax")
 today <- as.Date(Sys.timeDate())
+
+con <- dbConnect(RSQLite::SQLite(),"~/CampingWeather.db")
+
+NSWXList <- dbGetQuery(con, "SELECT Site_Name from RNS_data ORDER BY Site_Name")
+dbDisconnect(con)
 
 ui <- dashboardPage(
     dashboardHeader(title = "Camping Weather"),
@@ -18,8 +28,9 @@ ui <- dashboardPage(
     dashboardSidebar(
       sidebarMenu(
         dateInput("startDate", "Start Date", value = today),
+        selectInput("NSWXPick","Weather Station",NSWXList),
         menuItem("Map", tabName = "Map", icon = icon("map")),
-        menuItem("Temperature", tabName = "Temp", icon = icon("fas fa-temperature-low"))
+        menuItem("Synopsis", tabName = "Synop", icon = icon("far fa-file-alt"))
       )
     ),
   dashboardBody(
@@ -27,16 +38,18 @@ ui <- dashboardPage(
       # First tab content
       tabItem(tabName = "Map",
               fluidRow(
-                box(title = "Map",
-                  collapsible = TRUE,
-                  leafletOutput("NSmap")
-                  )
-              )
-      ),
+                box(title = "Map",collapsible = TRUE, leafletOutput("NSmap")),
+                box(title = "Temp",collapsible = TRUE, plotOutput("NStemp"))
+      )),
 
-      # Second tab content
-      tabItem(tabName = "Temp",
-              h2("Temperature data goes here")
+# Second tab content
+      tabItem(tabName = "Synop",
+              fluidRow(
+                box(title = "Synopsis",
+                    collapsible = TRUE,
+#                    plotOutput("NStemp")
+                )
+              )
       )
     )
 ))
@@ -44,15 +57,28 @@ ui <- dashboardPage(
 server <- function(input, output) {
 
   # Load the labels pulled from finding the weather stations via google street view
-  NS_PW_labels <- read.csv("/home/doug/RNS_data.csv",header = TRUE, stringsAsFactors = FALSE)
-
+  con <- dbConnect(RSQLite::SQLite(),"~/CampingWeather.db")
+  
+  NS_PW_labels <- dbGetQuery(con, "SELECT Site_Name, Long, Lat from RNS_data WHERE Data = 'x' ORDER BY Site_Name")
+  dbDisconnect(con)
+  
   output$NSmap <- renderLeaflet({
     # Use leaflet() here, and only include aspects of the map that
     # won't need to change dynamically (at least, not unless the
     # entire map is being torn down and recreated).
     leaflet(NS_PW_labels) %>% addProviderTiles(providers$Esri.WorldTopoMap) %>%
       setView(lng=-63.611, lat=44.5, zoom = 8) %>%
-      addMarkers(lng=NS_PW_labels$Long, lat=NS_PW_labels$Lat, popup=NS_PW_labels$Site_Name)
-})}
+      addMarkers(lng=as.numeric(NS_PW_labels$Long), lat=as.numeric(NS_PW_labels$Lat), popup=NS_PW_labels$Site_Name)
+  })
+  
+  output$NStemp <- renderPlot({
+    con <- dbConnect(RSQLite::SQLite(),"~/CampingWeather.db")
+    
+    NS_Temps <- dbGetQuery(con, "SELECT StationName, Year, Month, Day, MeanTemp_C from EC_dly_recs WHERE Month = 7")
+    dbDisconnect(con)
+    
+    hist(NS_Temps$MeanTemp_C)
+  })
+  }
 
 shinyApp(ui, server)
